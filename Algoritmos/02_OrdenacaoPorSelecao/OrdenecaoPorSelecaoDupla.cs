3f673c2d16e73b734b54b83922891709043879d3
Kelvyn Sinhorini <kelvynsinhorini03@gmail.com>
2024-07-20 11:26:25 -0300

    feat: adicionar algoritmo OrdenacaoPorSelecaoDupla (Double Selection Sort)

---
+++ Algoritmos/02_OrdenacaoPorSelecao/OrdenecaoPorSelecaoDupla.cs
@@ -0,0 +1,40 @@
+﻿namespace Algoritmos;
+
+public class OrdenecaoPorSelecaoDupla : IAlgoritmo
+{
+    public void Run()
+    {
+        var numbers = new int[] { 4, 5, 1, 3, 10, 9, 6, 8, 7, 2 };
+        var sortedArr = SelectionSort(numbers);
+
+        Console.WriteLine(string.Join(", ", numbers));
+        Console.WriteLine(string.Join(", ", sortedArr));
+    }
+
+    /// <summary>
+    /// O(n x n) ou O(n^2)
+    /// </summary>
+    /// <param name="array"></param>
+    /// <returns></returns>
+    private int[] SelectionSort(int[] array)
+        => SelectionSort(new LinkedList<int>(array)).ToArray();
+
+    private IEnumerable<int> SelectionSort(LinkedList<int> list)
+    {
+        var minList = new LinkedList<int>();
+        var maxList = new LinkedList<int>();
+
+        while (list.Count != 0)
+        {
+            var min = list.Min();
+            list.Remove(min);
+            minList.AddLast(min);
+
+            var max = list.Max();
+            list.Remove(max);
+            maxList.AddFirst(max);
+        }
+
+        return minList.Union(maxList);
+    }
+}
